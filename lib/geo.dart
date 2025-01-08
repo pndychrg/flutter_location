acbea74d40a4d8e27a04d6dca2e5d77e770ddd6a
Chirag Pandey <pndy.chrg2003@gmail.com>
2025-01-08 15:03:08 +0530

    added fetching state and district and much more details from zipcode

--- lib/geo.dart
+++ lib/geo.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class LocationFetcher extends StatefulWidget {
   const LocationFetcher({super.key});
@@ -79,6 +81,54 @@ class _LocationFetcherState extends State<LocationFetcher> {
     }
   }
 
+  TextEditingController zipcodeController = TextEditingController();
+
+  Future<Map<String, String>> fetchLocationDetails(String pincode) async {
+    final url =
+        'https://api.postalpincode.in/pincode/$pincode'; // API endpoint for Indian PIN codes
+
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        if (data[0]['Status'] == 'Success') {
+          final String city = data[0]['PostOffice'][0]['Division'];
+          final String state = data[0]['PostOffice'][0]['State'];
+
+          return {'city': city, 'state': state};
+        } else {
+          throw Exception('Invalid PIN code');
+        }
+      } else {
+        throw Exception('Failed to load location details');
+      }
+    } catch (e) {
+      throw Exception('Error fetching location details: $e');
+    }
+  }
+
+  String city = '';
+  String state = '';
+  void _onPincodeChanged(String pincode) async {
+    if (pincode.length == 6) {
+      // India has 6-digit PIN codes
+      try {
+        final locationDetails = await fetchLocationDetails(pincode);
+        setState(() {
+          city = locationDetails['city'] ?? '';
+          state = locationDetails['state'] ?? '';
+        });
+      } catch (e) {
+        setState(() {
+          city = '';
+          state = '';
+        });
+        print('Error fetching location details: $e');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -92,6 +142,21 @@ class _LocationFetcherState extends State<LocationFetcher> {
             Text(_locationMessage),
             SizedBox(height: 10),
             Text(_address),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: zipcodeController,
+              decoration: const InputDecoration(
+                labelText: 'PIN Code',
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.number,
+              onChanged: _onPincodeChanged,
+            ),
+            SizedBox(height: 20),
+            Text('City: $city'),
+            Text('State: $state'),
           ],
         ),
       ),
